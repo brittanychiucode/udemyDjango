85d3fb19900bc75a98d13f9a569e4fc2c5232fa1
Brittany Chiu <bc.chiu12@gmail.com>
2021-12-16 03:26:26 -0500

    urls and index

---
+++ my_site/blog/urls.py
@@ -0,0 +1,11 @@
+# Imports
+from django.urls import path
+
+# Views
+from . import views
+
+urlspatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page") #/posts/my-first-post
+]
